9ce497d0e9ad50f960c2bf742e72fe330de700e0
_ <_@_>
2021-05-18 20:44:55 +0800

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { Header } from "./components/Header";
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Content>
+      </Content>
+    </>
+  );
+};
+
+const GlobalStyle = createGlobalStyle`
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Rajdhani', Microsoft JhengHei, sans-serif;
+  background-color: #151515;
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+`;
+
+const Content = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+import ghLogo from "../../assets/github-logo.svg";
+import styled from "styled-components";
+
+export const Header = () => (
+  <Wrapper>
+    <Logo src={logo} alt="logo" />
+    <Title>Genetic Image Generation</Title>
+    <a href="https://github.com/marklai1998/genetic-image-generation">
+      <GHLogo src={ghLogo} alt="git-hub-logo" />
+    </a>
+  </Wrapper>
+);
+
+const Wrapper = styled.header`
+  background-color: #242424;
+  height: 50px;
+  flex-shrink: 0;
+  display: flex;
+`;
+
+const Logo = styled.img`
+  height: 30px;
+  padding: 10px;
+  background-color: #2d3034;
+  color: #e6e6e6;
+  float: left;
+`;
+
+const Title = styled.div`
+  float: left;
+  line-height: 50px;
+  color: #e6e6e6;
+  font-size: 20px;
+  padding-left: 15px;
+  font-weight: bold;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const GHLogo = styled.img`
+  float: right;
+  height: 30px;
+  padding: 10px;
+  background-color: #2d3034;
+  color: #e6e6e6;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: rgb(53, 53, 53);
+  }
+`;
