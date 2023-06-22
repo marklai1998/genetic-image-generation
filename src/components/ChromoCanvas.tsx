e52b8fb62bc991bba34277099ff92b6225cf4199
_ <_@_.com>
2023-06-21 22:47:25 -0400

    chore: update dependency

--- src/components/ChromoCanvas.tsx
+++ src/components/ChromoCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Chromo } from '../genetic/chromo'
 import { drawChromo } from '../genetic/utils'
 
@@ -10,30 +10,31 @@ type Props = {
   onDraw?: (chromo: Chromo, canvas: HTMLCanvasElement) => void
 }
 
-export const ChromoCanvas = forwardRef(
-  (
-    { containerWidth, containerHeight, chromo, maxWidth, onDraw }: Props,
-    ref: any
-  ) => {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+export const ChromoCanvas = ({
+  containerWidth,
+  containerHeight,
+  chromo,
+  maxWidth,
+  onDraw,
+}: Props) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
-    useEffect(() => {
-      const canvas = canvasRef.current
-      if (!canvas) return
-      const lowestDimension = maxWidth
-        ? Math.min(containerWidth, containerHeight, maxWidth)
-        : Math.min(containerWidth, containerHeight)
-      canvas.width = lowestDimension
-      canvas.height = lowestDimension
-    }, [containerWidth, containerHeight, maxWidth])
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+    const lowestDimension = maxWidth
+      ? Math.min(containerWidth, containerHeight, maxWidth)
+      : Math.min(containerWidth, containerHeight)
+    canvas.width = lowestDimension
+    canvas.height = lowestDimension
+  }, [containerWidth, containerHeight, maxWidth])
 
-    useEffect(() => {
-      const canvas = canvasRef.current
-      if (!canvas || !chromo) return
-      drawChromo(chromo, canvas)
-      onDraw && onDraw(chromo, canvas)
-    }, [chromo, onDraw])
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (!canvas || !chromo) return
+    drawChromo(chromo, canvas)
+    onDraw && onDraw(chromo, canvas)
+  }, [chromo, onDraw])
 
-    return <canvas ref={canvasRef} />
-  }
-)
+  return <canvas ref={canvasRef} />
+}
